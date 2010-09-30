f951bcf547db4bbae6d1692eff5bbd0ed06173ba
Julien Pecqueur (JPEC) <jpec@julienpecqueur.com>
2010-09-30 22:06:56 +0200

    Feature: Ajout table template.

---
+++ qfacture_update_ajout_template.sql
@@ -0,0 +1,8 @@
+CREATE TABLE  `qfacture_db`.`template` (
+  `Id` smallint(5) unsigned NOT NULL AUTO_INCREMENT COMMENT 'Id template',
+  `Type` varchar(10) NOT NULL COMMENT 'Type template',
+  `Sel` varchar(1) NOT NULL COMMENT 'Template sélectionné (pour le type)',
+  `Name` varchar(255) NOT NULL COMMENT 'Désignation template',
+  `Content` text NOT NULL COMMENT 'Contenu du template',
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='Table des templates de facture';
