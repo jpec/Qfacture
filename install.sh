12e897ef205536fe485e21160fc4cc642bd89538
Julien Pecqueur (JPEC) <jpec@julienpecqueur.com>
2010-10-29 18:53:22 +0200

    Add: script installation.

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+# install.sh - build the program & install it
+
+clear
+echo "***WARNING : "
+
+echo "*** Cleaning the project..."
+make clean
+
+echo "*** Generating makefile..."
+if [ -f /usr/bin/qmake-qt4 ];
+then 
+    echo "    Using qmake-qt4."
+    qmake-qt4 Qfacture.pro
+else
+    echo "    Using qmake."
+    qmake Qfacture.pro
+fi
+
+echo "*** Building the program..."
+make
+
+echo "*** Installing the program..."
+sudo cp bin/Qfacture /usr/local/bin/qfacture
+sudo cp art/qfacture.png /usr/share/pixmaps/qfacture.png
+sudo cp qfacture.desktop /usr/share/applications/qfacture.desktop
+
+echo "*** END OF INSTALLATION ***"
+echo " "
+echo "Now you have to configure you SQL database."
+echo " "
+echo "If /usr/local/bin is not in your PATH, you need to create "
+echo "a symbolic link in /usr/bin to /usr/local/bin/qfacture :"
+echo "cd /usr/bin && sudo ln -s /usr/local/bin/qfacture"
+echo " "
+echo "For more informations : http://github.com/jpec/Qfacture/wiki"
